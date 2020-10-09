0ef38ef5b799a301fe158709acada12973fffa32
par0-git <68587603+par0-git@users.noreply.github.com>
2020-10-09 16:44:35 +0800

    More work on JSON layer

--- src/Objects/SerializedContainer.h
+++ src/Objects/SerializedContainer.h
@@ -1,1 +1,31 @@
 #pragma once
+#include "SerializedValue.h"
+
+namespace LCU {
+	enum SerializedContainerChildType {
+		OBJECT,
+		CONTAINER,
+	};
+
+	struct SerializedContainerChild {
+		SerializedContainerChildType type;
+		union {
+			SerializedValue vObject;
+			SerializedContainer vContainer;
+		};
+	};
+
+	class SerializedContainer {
+	public:
+		SerializedContainer(std::string containerName) {
+			name = containerName;
+		}
+
+		SerializedContainerChild& Get() {
+
+		}
+	private:
+		std::string name;
+		std::vector<SerializedContainerChild> children;
+	};
+}
--- src/Output/Exceptions.hpp
+++ src/Output/Exceptions.hpp
@@ -1,3 +1,4 @@
 #pragma once
 #include "Exceptions/CurlFailure.hpp"
 #include "Exceptions/InvalidResponse.hpp"
+#include "Exceptions/SerializedValueFailure.hpp"
